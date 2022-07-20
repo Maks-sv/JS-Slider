d1f5cc18d39d43c750386918cf851e8fa1b94545
Maks-sv <maks.savov@gmail.com>
2022-07-20 20:17:08 +0300

    slider-class-formatting

--- assets/js/swipe-carousel.js
+++ assets/js/swipe-carousel.js
@@ -1,17 +1,17 @@
-import Carousel from './carousel.js';
-class SwipeCarousel extends Carousel{
+import Carousel from "./carousel.js";
+class SwipeCarousel extends Carousel {
   _initListeners() {
-  super._initListeners();
-    this.container.addEventListener('touchstart',this._swipeStart.bind(this));
-    this.container.addEventListener('touchend',this._swipeEnd.bind(this));
+    super._initListeners();
+    this.container.addEventListener("touchstart", this._swipeStart.bind(this));
+    this.container.addEventListener("touchend", this._swipeEnd.bind(this));
   }
-  _swipeStart (e) {
-    this.swipeStartX = e.changedTouches [0].pageX;
-    }
-_swipeEnd (e) {
-    this.swipeEndX = e.changedTouches [0].pageX;
-   if (this.swipeStartX - this.swipeEndX < -100)this.prev();
-   if (this.swipeStartX - this.swipeEndX > 100)this.next();
-    }
+  _swipeStart(e) {
+    this.swipeStartX = e.changedTouches[0].pageX;
+  }
+  _swipeEnd(e) {
+    this.swipeEndX = e.changedTouches[0].pageX;
+    if (this.swipeStartX - this.swipeEndX < -100) this.prev();
+    if (this.swipeStartX - this.swipeEndX > 100) this.next();
+  }
 }
 export default SwipeCarousel;
